0bab34beb9ae9a1fde9216a484621892a6e25695
julesbertrand <julesbertrand13@gmail.com>
2024-04-11 18:51:11 +0200

    chore: change ping url

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 GCS_BUCKET = "elo-match"
 DEFAULT_ELO = 1500
 
-PING_URL = "https://ping-5a442voexq-ew.a.run.app"
+PING_URL = "https://ping-r4ofkdaycq-ew.a.run.app"
 ENV=os.getenv("ENV", "dev")
